3fffb01514f2a975278dc720cf4c46a0649520ce
Dolev Levy <Dolevlevy97@gmail.com>
2024-07-14 21:56:48 +0300

    Implement toy images

--- src/cmps/ToyPreview.jsx
+++ src/cmps/ToyPreview.jsx
@@ -4,6 +4,7 @@ export function ToyPreview({ toy }) {
         <article className="toy-preview">
             <h2>{toy.name}</h2>
             <p>${toy.price}</p>
+            <img src={toy.img} alt="toy image" />
             <p>{toy.inStock ? 'In stock' : 'Out of stock'}</p>
         </article>
     )
--- src/pages/ToyDetails.jsx
+++ src/pages/ToyDetails.jsx
@@ -44,7 +44,7 @@ export function ToyDetails() {
             <h1>{toy.name}</h1>
             <h4>{toy.labels.join(', ')} </h4>
             <hr />
-            <img className="toy-image" src="https://images.unsplash.com/photo-1581557991964-125469da3b8a?q=80&w=1433&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" title="Image" />
+            <img className="toy-image" src={toy.img} title="Image" />
             <p>${toy.price}</p>
             <h3>{toy.inStock ? 'Currently in stock!' : 'Out of stock'}</h3>
             <Button size="large" variant="contained" className="btn btn-checkout">{toy.inStock ? 'Buy now!' : "Notify me when available"}</Button>
